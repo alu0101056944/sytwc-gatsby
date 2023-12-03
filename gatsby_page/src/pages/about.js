a64878a5e2b00cdd0c16c1cbc85b7f4724e5ba10
alu0101056944@ull.edu.es <alu0101056944@ull.edu.es>
2023-12-03 16:34:02 +0000

    About page finished.

--- gatsby_page/src/pages/about.js
+++ gatsby_page/src/pages/about.js
@@ -1,10 +1,14 @@
 import * as React from 'react';
 import PageBody from '../templates/pagebody';
 import PageBodyAdvanced from '../templates/pagebody-advanced';
+import { StaticImage } from 'gatsby-plugin-image';
 
 const About = () => {
   return (
     <PageBodyAdvanced>
+      <p>Marcos Jesús Barrios Lorenzo</p>
+      <p>Creado el 03/12/2023</p>
+      <StaticImage src="../images/author-picture.jpg" alt="Author image taken from it's github account"/>
       <p>En la asignatura de <em>Sistemas y Tecnologías Web: Cliente</em> se aprenden conceptos
       relacionados con tecnologías web utilizadas en empresas del mercado laboral. Es importante
       conocerlas porque este último está sometido a restricciones a los que un entorno académico
