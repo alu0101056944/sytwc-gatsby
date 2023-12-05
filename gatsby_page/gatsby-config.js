54959911a1954f9f9a56b6150e057443ca51c678
alu0101056944@ull.edu.es <alu0101056944@ull.edu.es>
2023-12-05 18:11:29 +0000

    Failed attempt at making Building build a placeholder content array.

--- gatsby_page/gatsby-config.js
+++ gatsby_page/gatsby-config.js
@@ -4,7 +4,7 @@
 module.exports = {
   siteMetadata: {
     title: `SYTWC Gatsby Assignment`,
-    siteUrl: `https://www.yourdomain.tld`
+    siteUrl: `https://www.yourdomain.tld`,
   },
   plugins: [
     "gatsby-plugin-sass",
@@ -13,8 +13,9 @@ module.exports = {
     {
       resolve: 'gatsby-source-filesystem',
       options: {
-        name: 'all page components',
-        path: `${__dirname}/src/pages`
+        name: 'bienes',
+        path: `${__dirname}/src/assets/goods/`,
+        ignore: [`**/*.!(json)`]
       }
     }
   ]
--- gatsby_page/src/templates/building/building.js
+++ gatsby_page/src/templates/building/building.js
@@ -1,18 +1,44 @@
 import * as React from 'react';
 import '../../styles/building/building.scss';
 import Placeholder from './placeholder';
+import Card from './card';
+import { StaticImage } from 'gatsby-plugin-image';
+
+import goods from '../../../content/bienes.json';
+
+const createCardFromGood = (good) => (
+  <Card>
+    <StaticImage src={good.img} alt={"Foto del bien cultural."}/>
+    <h2>{good.nombre}</h2>
+    <p>{good.antecedentes}</p>
+    <p>Tipo: {good.tipo.arquitectura}</p>
+    <p>Época: {good.tipo.época}</p>
+    <p>Localización: {`lat ${good.localizacion.lat}, long: ${good.localizacion.long}`}</p>
+  </Card>
+)
 
 const Building = ({amountOfPlaceholders = 0, children}) => {
+  const [allPlaceholderContent] = React.useState([]);
+
+  React.useEffect(() => {
+        goods.bienes.forEach(bien => {
+              allPlaceholderContent.push(createCardFromGood(bien))
+            });
+      }, []); // empty array means only run this once per component mount
+
+  React.useEffect(() => {
+        allPlaceholderContent.concat(React.Children.toArray(children));
+      }, []);
+
   return (
     <div className="placeholders">
       {
-        React.Children.toArray(children)
-            .reduce((allPlaceholder, child) => {
+       allPlaceholderContent
+            .reduce((allPlaceholder, content) => {
                   if (allPlaceholder.length < amountOfPlaceholders) {
-                    const KEY =
-                        `placeholder-${allPlaceholder.length}`;
+                    const KEY = `placeholder-${allPlaceholder.length}`;
                     allPlaceholder.push((
-                          <Placeholder key={KEY}>{child}</Placeholder>
+                          <Placeholder key={KEY}>{content}</Placeholder>
                         ));
                   }
                   return allPlaceholder;
