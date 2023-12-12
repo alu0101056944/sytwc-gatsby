f9cbb1bb08e37c11cd06f011e36e06640141f81b
alu0101056944@ull.edu.es <alu0101056944@ull.edu.es>
2023-12-12 11:19:18 +0000

    Building now receives the goods as prop.

--- gatsby_page/src/pages/index.js
+++ gatsby_page/src/pages/index.js
@@ -2,11 +2,13 @@ import * as React from 'react';
 import PageBodyAdvanced from '../templates/pagebody-advanced';
 import Building from '../templates/building/building';
 
+import goods from '../../../content/bienes.json';
+
 const IndexPage = () => {
   return (
     <>
       <PageBodyAdvanced>
-        <Building amountOfPlaceholders={5}/>
+        <Building amountOfPlaceholders={5} goods={goods.bienes}/>
       </PageBodyAdvanced>
     </>
   )
