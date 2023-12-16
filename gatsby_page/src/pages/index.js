1028987c8ca8f8baf904a2c27027288e8e191822
alu0101056944@ull.edu.es <alu0101056944@ull.edu.es>
2023-12-16 16:53:45 +0000

    Added ContentAndSidebar template to index.js. Left BuildingWithAPI out. Pending introducing sidebar form.

--- gatsby_page/src/pages/index.js
+++ gatsby_page/src/pages/index.js
@@ -1,9 +1,11 @@
 import * as React from 'react';
+
 import PageBodyAdvanced from '../templates/structure/pagebody-advanced';
 import Building from '../templates/building/building';
+import BuildingWithAPI from '../templates/building/building_with_api';
+import ContentAndSidebar from '../templates/structure/content-and-sidebar';
 
 import goods from '../../content/bienes.json';
-import BuildingWithAPI from '../templates/building/building_with_api';
 
 const IndexPage = () => {
   const contentTransform = (bien) => {
@@ -21,16 +23,16 @@ const IndexPage = () => {
         )
       }
   return (
-    <>
-      <PageBodyAdvanced>
-        <h3>Usos del componente edificio</h3>
-        <h4>Con datos obtenidos de archivo .json:</h4>
+    <PageBodyAdvanced>
+      <h1>Usos del componente edificio</h1>
+      <ContentAndSidebar>
+        <h2>Con datos obtenidos de archivo .json:</h2>
         <Building amountOfPlaceholders={5} content={goods.bienes}
             contentTransform={contentTransform}/>
-        <h4>Con datos de API</h4>
-        <BuildingWithAPI amountOfPlaceholders={12}></BuildingWithAPI>
-      </PageBodyAdvanced>
-    </>
+      </ContentAndSidebar>
+      <h2>Con datos de API</h2>
+      <BuildingWithAPI amountOfPlaceholders={12}></BuildingWithAPI>
+    </PageBodyAdvanced>
   )
 }
 
